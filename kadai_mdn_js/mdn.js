6e8873ae31033db589fb26372ea6cb9123d9f889
yutak215 <31120122+yutak215@users.noreply.github.com>
2024-06-16 08:51:56 +0900

    Add files via upload
    mdn_jsをプッシュ
    月の表示の調整

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,7 +1,7 @@
 const displayDate = () => {
     const today = new Date();
     let thisYear = (today.getFullYear() + '年');
-    let thisMonth = (today.getMonth() + '月');
+    let thisMonth = ((today.getMonth() + 1) + '月');
     let thisDate = (today.getDate() + '日');
     console.log(thisYear + thisMonth + thisDate);
 }
